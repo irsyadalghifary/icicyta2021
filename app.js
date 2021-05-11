2cae94c5ae82d3f6b8845ce5e4c4cc9b650bdab1
irsyadalghifary <alghifary64@gmail.com>
2021-05-11 15:09:56 +0700

    update lagi

---
+++ app.js
@@ -0,0 +1,51 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 1, 2021 20:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("c-days").innerHTML =
+    days + "<div class='small-text'>Days</div>";
+  document.getElementById("c-hours").innerHTML =
+    hours + "<div class='small-text'>Hours</div>";
+  document.getElementById("c-minutes").innerHTML =
+    minutes + "<div class='small-text'>Minutes</div>";
+  document.getElementById("c-seconds").innerHTML =
+    seconds + "<div class='small-text'>Seconds</div>";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML =
+      "<h1>Today is The Day <span style='color:#ED3A8D;'>!</span></h1>";
+  }
+}, 1000);
+
+function openCard(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
